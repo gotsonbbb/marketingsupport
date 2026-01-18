dbe28da6e60b8d9e8ad0d6b79c5d21bab21f96dc
marketingsupportpro <gotson125@gmail.com>
2026-01-18 22:39:03 +0630

    init MKTSP

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.marketingsupport.app',
+  appName: 'MKTSP',
+  webDir: 'dist'
+};
+
+export default config;
