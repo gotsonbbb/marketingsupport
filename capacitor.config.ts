edff5115730e0829fce6e18ec4f70f20b2d68d7c
marketingsupportpro <gotson125@gmail.com>
2026-01-19 09:35:03 +0630

    upload file

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.marketingsupport.app',
   appName: 'MKTSP',
-  webDir: 'dist'
+  webDir: 'dist',
 };
 
 export default config;
